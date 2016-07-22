f8b97ef395f3c0934b0b77df41f46eadc21509db
rowana16 <aaron.rowan@gmail.com>
2016-07-22 15:43:01 -0400

    New Scaffolding

---
+++ CFPortfolio/Models/codeFirst/BlogPost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CFPortfolio.Models.codeFirst
+{
+    public class BlogPost
+    {
+        public BlogPost()
+        {
+            this.Comments = new HashSet<Comment>();
+        }
+
+        public int Id { get; set; }
+        public DateTimeOffset Created{ get; set; }
+        public DateTimeOffset? Updated { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required()]
+        public string Body { get; set; }
+        public string MediaURL { get; set; }
+        public bool Published { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+        
+
+    }
+}
